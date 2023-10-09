5d56ae3167fe5626b361875f422be2c638acdde0
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2023-10-09 00:26:54 -0600

    Capacitor esferico y cilindrico sin dielectricos, ni dibujito

--- Parcial2.py
+++ Parcial2.py
@@ -16,8 +16,9 @@ from tkinter import *
 from tkinter import messagebox
 from math import sin, cos, tan 
 from math import pi
+from math import log
 from tkinter.ttk import Combobox 
-from sympy import *  #libreria para las derivadas e integrales
+#from sympy import *  #libreria para las derivadas e integrales
 
 
 
@@ -39,17 +40,17 @@ class app(Tk):
         #self.lv=Label(text="Ingrese el voltaje");self.lv.place(x=10,y=130); self.lv.config(bg="#ffc2d1")
         #self.ev=Entry(self);self.ev.place(x=10,y=160)
 
-        #self.b = Button(self, text = "Crear",command= self.PlacasGUI, width=20, bg="#ff8fab"); self.b.place(x=20, y =130)
         self.b = Button(self, text = "Crear",command= self.llamar, width=20, bg="#ff8fab"); self.b.place(x=10, y =190)
-
-
 
 
 
     def llamar(self): # para llamar a las diferentes pantallas
         if(self.caja.get()=="Placas paralelas"):
             self.PlacasGUI()
-        #else if 
+        elif (self.caja.get()=="Esferico"):
+            self.EsfericoGUI()
+        else:
+            self.CilindricoGUI()
 
 
 
@@ -97,8 +98,87 @@ class app(Tk):
         self.c1.config(bg="misty rose")
 
 
-    #pedir: dimensiones, voltaje y si es dielectrico
+    def EsfericoGUI(self):
+        self.ventana2 = Toplevel(self)
+        self.ventana2.geometry("1400x1400")
+        self.ventana2.config(bg="#ffafcc")
+        #objetos
 
+        self.l1=Label(self.ventana2,text="Radio A interno (m):");self.l1.place(x=10,y=10); self.l1.config(bg="#ffc2d1")
+        self.e1=Entry(self.ventana2);self.e1.place(x=160,y=10)
+
+        self.l2=Label(self.ventana2,text="Radio B externo (m):");self.l2.place(x=10,y=50); self.l2.config(bg="#ffddd2")
+        self.e2=Entry(self.ventana2);self.e2.place(x=160,y=50)
+
+        self.l3=Label(self.ventana2,text="voltaje (V):");self.l3.place(x=10,y=90); self.l3.config(bg="#ff8fab")
+        self.e3=Entry(self.ventana2);self.e3.place(x=160,y=90)
+        
+        self.btn1= Button(self.ventana2, text="Capacitor esférico", width=35,command=self.Esferico, bg="#ffc2d1");self.btn1.place(x=350,y=10)
+        self.btn2= Button(self.ventana2, text="Capacitor esférico con dielectrico a la mitad", width=35,command=self.EsfericoMitad, bg="#ffb3c6");self.btn2.place(x=350,y=40)
+        self.btn3= Button(self.ventana2, text="Capacitor esférico con dielectrico completo", width=35,command=self.EsfericoCompleto, bg="#ff8fab");self.btn3.place(x=350,y=70)       
+        
+        #self.l5=Label(self.ventana, text="");self.l5.place(x=10,y=170); self.l5.config(bg="#ff8fab")
+        self.l6=Label(self.ventana2, text="capacitancia (F):");self.l6.place(x=10,y=170); self.l6.config(bg="#ffc2d1")
+        self.l7=Label(self.ventana2, text="");self.l7.place(x=10,y=210); self.l7.config(bg="#ff8fab")
+        self.l8=Label(self.ventana2, text="carga del capacitor");self.l8.place(x=10,y=250); self.l8.config(bg="#ffc2d1")
+        self.l9=Label(self.ventana2, text="");self.l9.place(x=10,y=290); self.l9.config(bg="#ff8fab")
+        self.l10=Label(self.ventana2, text="energía almacenada");self.l10.place(x=10,y=320); self.l10.config(bg="#ffc2d1")
+        self.l11=Label(self.ventana2, text="");self.l11.place(x=10,y=350); self.l11.config(bg="#ff8fab")
+        self.l12=Label(self.ventana2, text="");self.l12.place(x=10,y=390); self.l12.config(bg="#ffc2d1")
+        self.l13=Label(self.ventana2, text="");self.l13.place(x=10,y=430); self.l13.config(bg="#ff8fab")
+        self.l14=Label(self.ventana2, text="");self.l14.place(x=10,y=470); self.l14.config(bg="#ffc2d1")
+        self.l15=Label(self.ventana2, text="");self.l15.place(x=10,y=510); self.l15.config(bg="#ff8fab")
+        self.l16=Label(self.ventana2, text="");self.l16.place(x=10,y=550); self.l16.config(bg="#ffc2d1")
+        self.l17=Label(self.ventana2, text="");self.l17.place(x=10,y=590); self.l17.config(bg="#ff8fab")
+        #canvas
+        self.c1 = Canvas(self.ventana2, width=800, height=500, bg="white")
+        self.c1.place(x=350, y=150)
+        self.c1.config(bg="misty rose")
+
+    def CilindricoGUI(self):
+        self.ventana3 = Toplevel(self)
+        self.ventana3.geometry("1400x1400")
+        self.ventana3.config(bg="#ffafcc")
+        #objetos
+
+        self.l1=Label(self.ventana3,text="Radio A interno (m):");self.l1.place(x=10,y=10); self.l1.config(bg="#ffc2d1")
+        self.e1=Entry(self.ventana3);self.e1.place(x=160,y=10)
+
+        self.l2=Label(self.ventana3,text="Radio B externo (m):");self.l2.place(x=10,y=50); self.l2.config(bg="#ffddd2")
+        self.e2=Entry(self.ventana3);self.e2.place(x=160,y=50)
+
+        self.l3=Label(self.ventana3,text="largo (m):");self.l3.place(x=10,y=90); self.l3.config(bg="#ff8fab")
+        self.e3=Entry(self.ventana3);self.e3.place(x=160,y=90)
+
+        self.l4=Label(self.ventana3,text="voltaje (V):");self.l4.place(x=10,y=130); self.l4.config(bg="#f08080")
+        self.e4=Entry(self.ventana3);self.e4.place(x=160,y=130)
+        
+        self.btn1= Button(self.ventana3, text="Capacitor cilindrico", width=35,command=self.Cilindrico, bg="#ffc2d1");self.btn1.place(x=350,y=10)
+        self.btn2= Button(self.ventana3, text="Capacitor cilindrico con dielectrico a la mitad", width=35,command=self.CilindricoMitad, bg="#ffb3c6");self.btn2.place(x=350,y=40)
+        self.btn3= Button(self.ventana3, text="Capacitor cilindrico con dielectrico completo", width=35,command=self.CilindricoCompleto, bg="#ff8fab");self.btn3.place(x=350,y=70)       
+        
+        #self.l5=Label(self.ventana, text="");self.l5.place(x=10,y=170); self.l5.config(bg="#ff8fab")
+        self.l6=Label(self.ventana3, text="capacitancia (F):");self.l6.place(x=10,y=170); self.l6.config(bg="#ffc2d1")
+        self.l7=Label(self.ventana3, text="");self.l7.place(x=10,y=210); self.l7.config(bg="#ff8fab")
+        self.l8=Label(self.ventana3, text="carga del capacitor");self.l8.place(x=10,y=250); self.l8.config(bg="#ffc2d1")
+        self.l9=Label(self.ventana3, text="");self.l9.place(x=10,y=290); self.l9.config(bg="#ff8fab")
+        self.l10=Label(self.ventana3, text="energia almacenada");self.l10.place(x=10,y=320); self.l10.config(bg="#ffc2d1")
+        self.l11=Label(self.ventana3, text="");self.l11.place(x=10,y=350); self.l11.config(bg="#ff8fab")
+        self.l12=Label(self.ventana3, text="");self.l12.place(x=10,y=390); self.l12.config(bg="#ffc2d1")
+        self.l13=Label(self.ventana3, text="");self.l13.place(x=10,y=430); self.l13.config(bg="#ff8fab")
+        self.l14=Label(self.ventana3, text="");self.l14.place(x=10,y=470); self.l14.config(bg="#ffc2d1")
+        self.l15=Label(self.ventana3, text="");self.l15.place(x=10,y=510); self.l15.config(bg="#ff8fab")
+        self.l16=Label(self.ventana3, text="");self.l16.place(x=10,y=550); self.l16.config(bg="#ffc2d1")
+        self.l17=Label(self.ventana3, text="");self.l17.place(x=10,y=590); self.l17.config(bg="#ff8fab")
+        
+        #canvas
+        self.c1 = Canvas(self.ventana3, width=800, height=500, bg="white")
+        self.c1.place(x=350, y=150)
+        self.c1.config(bg="misty rose")
+
+
+
+    #pedir: dimensiones, voltaje y si es dielectrico
     #propiedades fisicas: 
     #capacitancia, carga del capacitor, energia almacenada del capacitor, 
     #si tiene dielectrico: valor de la carga libre y ligada
@@ -165,9 +245,7 @@ class app(Tk):
                 messagebox.showerror("Error", "Ingrese valores validos para la distancia y longitud")
         except Exception as msg: 
             messagebox.showerror("error", "asegurese de ingresar un número válido y todos los valores ")
-                
-
-        
+                       
 
     def PlacasParalelasDielectricoMitad(self):
         """
@@ -369,11 +447,135 @@ class app(Tk):
 
 
 
-
     def Esferico(self):
+        """
+        Ecuaciones utiles: 
+        C= 4pi*E0/((1/a)-(1/b))
+        Q=C*Vab
+        E= (1/2)*CV^2
+        """
+        try:
+
+            self.EpsilonCero = 8.85*10**-12
+            self.radioA= float(self.e1.get())
+            self.radioB= float(self.e2.get())
+            #self.largo= float(self.e3.get())
+            self.voltaje= float(self.e3.get())
+            self.plexiglas= float(3.40)
+
+            if (self.radioA > 0 and self.radioB > 0):
+                #-----limpiar canvas-----
+                self.limpiar()
+                #-----calculo de capacitancia------
+                """
+                C= 4pi*E0/((1/a)-(1/b))
+                """
+                self.Capacitancia= (self.EpsilonCero * 4 * pi)/((1/self.radioA)-(1/self.radioB))
+                self.l7.config(text=str( self.Capacitancia))
+
+                #------carga del capacitor ------
+                """
+                Q=C*Vab
+                """
+                
+                self.carga= self.Capacitancia *  self.voltaje   
+                self.l8.config(text="carga del capacitor (C):")
+                self.l9.config(text=str(self.carga))
+
+                #------energia------
+                """
+                E= (1/2)*CV^2
+                """
+                self.Energia = (1/2)*(self.Capacitancia*(self.voltaje**2) )
+                self.l10.config(text="energia almacenada (J):")
+                self.l11.config(text=str(self.Energia))
+
+                self.l12.config(text=" ")
+                self.l13.config(text=" ")
+                self.l14.config(text=" ")
+                self.l15.config(text=" ")
+                self.l16.config(text=" ")
+                self.l17.config(text=" ")
+
+
+
+            else: 
+                messagebox.showerror("Error", "Ingrese valores validos para la distancia y longitud")
+        except Exception as msg: 
+            messagebox.showerror("error", "asegurese de ingresar un número válido y todos los valores ")
+
+
+
+    def EsfericoMitad(self):
         pass
 
+    def EsfericoCompleto(self):
+        pass
+
+
+
     def Cilindrico(self):
+        #no esta terminado
+        """
+        Ecuaciones utiles: 
+        C= 2pi*E0*L/ln(b/a)
+        Q=C*Vab
+        E= (1/2)*CV^2
+        """
+        try:
+
+            self.EpsilonCero = 8.85*10**-12
+            self.radioA= float(self.e1.get())
+            self.radioB= float(self.e2.get())
+            self.largo= float(self.e3.get())
+            self.voltaje= float(self.e4.get())
+            self.plexiglas= float(3.40)
+
+            if (self.radioA > 0 and self.radioB > 0 and  self.largo > 0):
+                #-----limpiar canvas-----
+                self.limpiar()
+                #-----calculo de capacitancia------
+                """
+                C= 2pi*E0*L/ln(b/a)
+                """
+                self.Capacitancia= (self.EpsilonCero * self.largo * 2 * pi)/log(self.radioB/self.radioA)
+                self.l7.config(text=str( self.Capacitancia))
+
+                #------carga del capacitor ------
+                """ 
+                Q=C*Vab
+                """
+                
+                self.carga= self.Capacitancia *  self.voltaje   
+                self.l8.config(text="carga del capacitor (C):")
+                self.l9.config(text=str(self.carga))
+
+                #------energia------
+                """
+                E= (1/2)*CV^2
+                """
+                self.Energia = (1/2)*(self.Capacitancia*(self.voltaje**2) )
+                self.l10.config(text="energia almacenada (J):")
+                self.l11.config(text=str(self.Energia))
+
+                self.l12.config(text=" ")
+                self.l13.config(text=" ")
+                self.l14.config(text=" ")
+                self.l15.config(text=" ")
+                self.l16.config(text=" ")
+                self.l17.config(text=" ")
+
+
+
+            else: 
+                messagebox.showerror("Error", "Ingrese valores validos para la distancia y longitud")
+        except Exception as msg: 
+            messagebox.showerror("error", "asegurese de ingresar un número válido y todos los valores ")
+
+    def CilindricoMitad(self):
+        pass
+
+    def CilindricoCompleto(self):
         pass
                   
 
